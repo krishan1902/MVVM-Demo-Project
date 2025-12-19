106317df3179c999a054bee398ed6b43676cd8f5
Krishan1902 <krishan@gmx.at>
2025-12-19 23:13:03 +0100

    first commit, added gui without binding

---
+++ WPFDemo/Models/Person.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPFDemo.Models
+{
+    public record Person
+    {
+        //record -> performanter, für DTOs (DataTranferObjects) ausgelegt, halten nur Daten, Attribute
+        //          nicht für Methoden
+        //init   -> nur für einmalige zuweisen
+
+        public String Vorname { get; init; }
+        public String Nachname { get; init; }
+        public int Alter {  get; init; }
+
+        public Person(string vorname, string nachname, int alter)
+        {
+            Vorname = vorname;
+            Nachname = nachname;
+            Alter = alter;
+        }
+
+        
+    }
+}
